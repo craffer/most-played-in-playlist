bf7a1ebf1c5178e5328134585f920000a6cd0d24
Conor Rafferty <craffer@umich.edu>
2021-09-28 23:23:52 -0700

    Install spotipy and pre-commit

--- src/main.py
+++ src/main.py
@@ -1,23 +1,24 @@
 import os
 import pylast
 
-API_KEY = os.environ.get("API_KEY")
-API_SECRET = os.environ.get("API_SECRET")
+LASTFM_API_KEY = os.environ.get("API_KEY")
+LASTFM_API_SECRET = os.environ.get("API_SECRET")
 LAST_FM_USERNAME = os.environ.get("LAST_FM_USERNAME")
 LAST_FM_PASSWORD = pylast.md5(os.environ.get("LAST_FM_PASSWORD"))
 
+
 lastfm = pylast.LastFMNetwork(
-    api_key=API_KEY,
-    api_secret=API_SECRET,
+    api_key=LASTFM_API_KEY,
+    api_secret=LASTFM_API_SECRET,
     username=LAST_FM_USERNAME,
     password_hash=LAST_FM_PASSWORD,
 )
 
 user = lastfm.get_user(LAST_FM_USERNAME)
-top_tracks= user.get_top_tracks(pylast.PERIOD_6MONTHS, limit=300)
+top_tracks = user.get_top_tracks(pylast.PERIOD_6MONTHS, limit=300)
 
 print("Your top played tracks of the last 6 months:")
 for i, track in enumerate(top_tracks):
-    if (track.weight < 5):
+    if track.weight < 5:
         break
     print(f"{i + 1}. {track.item} – {track.weight} plays")
